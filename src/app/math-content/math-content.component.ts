0c05a332bad1025d435c6a597215cae200ab0b32
Cliff Han(Xian) <cliff.han@kaiostech.com>
2019-06-17 18:03:10 +0800

    Add temporary math for use

--- src/app/math-content/math-content.component.ts
+++ src/app/math-content/math-content.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MathContentComponent implements OnInit {
   mathArray = [];
-  maxNumber = 5;
+  maxNumber = 100;
 
   constructor() { }
 
@@ -17,12 +17,22 @@ export class MathContentComponent implements OnInit {
 
   exec(maxNumber) {
     this.mathArray = [];
-    let i, j;
-    for(i = 1; i <= maxNumber; i++) {
-      for (j = 1; j <= maxNumber; j++) {
-        this.mathArray.push({x:i, y:j});
+    let i;
+    for (i = 1; i <= 20; i++) {
+      let x = Math.floor(Math.random() * 100) + 1;
+      let y = Math.floor(Math.random() * 100) + 1;
+      if (x > y) {
+        this.mathArray.push({x:x, y:y});
+      }
+      else {
+        this.mathArray.push({x:y, y:x});
       }
     }
+    // for(i = 1; i <= maxNumber; i++) {
+    //   for (j = 1; j <= maxNumber; j++) {
+    //     this.mathArray.push({x:i, y:j});
+    //   }
+    // }
   }
 
 }
